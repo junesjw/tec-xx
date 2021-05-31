e6960612857eaf1b4d27a7d5f003f7690b593a12
AlPalmone <70707583+AlPalmone@users.noreply.github.com>
2021-06-01 00:42:52 +0200

    Added helper message that posts a link to the pastebin with all commands if the character command was not found

--- Commands/CharacterCommands.cs
+++ Commands/CharacterCommands.cs
@@ -17,7 +17,8 @@ namespace tec_xx.Commands
         private const string docHeadUrl = "https://www.ssbwiki.com/images/c/c8/DrMarioHeadSSBUWebsite.png";
         private const string docStop = "https://docs.google.com/spreadsheets/d/1f00Ph9bDgF5Nc0GFokGsmuTsRUnBmLRj8aN-yeXPy4g/edit?usp=sharing";
         private const string footer = "The tips were given by Junes#9469 and/or Labrys#4412 on Discord.\nPlease contact us or the mod team if you have any suggestions!";
-        
+        private const string pastebin = "https://pastebin.com/8Aw08aBv";
+
 
         [Command("vs")]
         public async Task SendMatchUpTips(CommandContext ctx, string character)
@@ -30,80 +31,79 @@ namespace tec_xx.Commands
             if (!await _db.CharacterExists(character, "Character", "MatchupAdvice"))
             ***REMOVED***
                 if (await _db.CharacterExists(character, "Alias", "Aliases"))
-                    character = await _db.LoadCharacterAccessName(character);
-                else
-                    Console.WriteLine("Character not found!");
-          ***REMOVED***
-
-            try
-            ***REMOVED*** 
-                var advice = await _db.LoadData<MatchupAdvice, dynamic>("MatchupAdvice", character, typeof(MatchupAdvice).GetProperties(), new ***REMOVED*** ***REMOVED***);
-
-                string headIconDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "HeadIcons");
-
-                // Robin already contains the word "rob" so I'm hardcoding it
-                if (!string.Equals(character, "rob"))
-                    headIconFileName = Directory.GetFiles(headIconDirectory).Where(s => s.ToLower().Contains(character)).ToList();
-                else
-                    headIconFileName.Add(string.Concat(headIconDirectory, @"\ROBHeadSSBUWebsite.png"));
-
-                Console.WriteLine(headIconFileName[0]);
-
-
-                using (var fs = new FileStream(headIconFileName[0], FileMode.Open))
                 ***REMOVED***
-                    var embed = new DiscordEmbedBuilder
-                    ***REMOVED***
-                        Title = $"Dr. Mario vs. ***REMOVED***advice.Title***REMOVED***",
-                        Url = docStop,
-                        Author = new DiscordEmbedBuilder.EmbedAuthor
-                        ***REMOVED***
-                            IconUrl = docHeadUrl,
-                            Name = author
-                      ***REMOVED***,
-                        Footer = new DiscordEmbedBuilder.EmbedFooter
-                        ***REMOVED***
-                            IconUrl = docHeadUrl,
-                            Text = footer
-                      ***REMOVED***,
-                        Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
-                        ***REMOVED***
-                            Url = $"attachment://***REMOVED***Path.GetFileName(headIconFileName[0])***REMOVED***"
-                      ***REMOVED***,
-
-                        Color = new Optional<DiscordColor>(new DiscordColor(advice.Color)),
-                  ***REMOVED***;
-
-                    if (!string.IsNullOrEmpty(advice.SecondaryNotes))
-                    ***REMOVED***
-                        string[] notesTitles = ReturnNotesTitle(advice);
-
-                        embed.AddField(notesTitles[0], BuildNotes(advice));
-
-                        embed.AddField(notesTitles[1], string.Concat("```\n", advice.SecondaryNotes, "```"));
-
-                        if(!string.IsNullOrEmpty(advice.TertiaryNotes))
-                            embed.AddField(notesTitles[2], string.Concat("```\n", advice.TertiaryNotes, "```"));
-                  ***REMOVED***
-                    else
-                    ***REMOVED***
-                        embed.AddField("Notes", BuildNotes(advice));
-                  ***REMOVED***
-
-                    embed.AddField("Stages (ordered from best to worst)", BuildStages(advice));
-                    embed.AddField("Documents", BuildOtherFields(advice.Document));
-                    embed.AddField("Videos", BuildOtherFields(advice.Video));
-                    embed.AddField("Other", BuildOtherFields(advice.Other));
-
-                    var msg = await new DiscordMessageBuilder()
-                            .WithEmbed(embed)
-                            .WithFiles(new Dictionary<string, Stream>() ***REMOVED*** ***REMOVED*** $"***REMOVED***Path.GetFileName(headIconFileName[0])***REMOVED***", fs ***REMOVED*** ***REMOVED***)
-                            .SendAsync(ctx.Channel);
+                    character = await _db.LoadCharacterAccessName(character);
               ***REMOVED***
+                else
+                ***REMOVED***
+                    var msg = new DiscordMessageBuilder()
+                        .WithContent($"You didn't enter a valid character argument. \n For a comprehensive list about all character commands, please see this ***REMOVED***pastebin***REMOVED*** link.")
+                        .SendAsync(ctx.Channel);
+
+                    return;
+              ***REMOVED***
+                    
           ***REMOVED***
-            catch (Exception e)
+
+            var advice = await _db.LoadData<MatchupAdvice, dynamic>("MatchupAdvice", character, typeof(MatchupAdvice).GetProperties(), new ***REMOVED*** ***REMOVED***);
+
+            string headIconDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "HeadIcons");
+
+            // Robin already contains the word "rob" so I'm hardcoding it
+            if (!string.Equals(character, "rob"))
+                headIconFileName = Directory.GetFiles(headIconDirectory).Where(s => s.ToLower().Contains(character)).ToList();
+            else
+                headIconFileName.Add(string.Concat(headIconDirectory, @"\ROBHeadSSBUWebsite.png"));
+
+            using (var fs = new FileStream(headIconFileName[0], FileMode.Open))
             ***REMOVED***
-                Console.WriteLine(e);
+                var embed = new DiscordEmbedBuilder
+                ***REMOVED***
+                    Title = $"Dr. Mario vs. ***REMOVED***advice.Title***REMOVED***",
+                    Url = docStop,
+                    Author = new DiscordEmbedBuilder.EmbedAuthor
+                    ***REMOVED***
+                        IconUrl = docHeadUrl,
+                        Name = author
+                  ***REMOVED***,
+                    Footer = new DiscordEmbedBuilder.EmbedFooter
+                    ***REMOVED***
+                        IconUrl = docHeadUrl,
+                        Text = footer
+                  ***REMOVED***,
+                    Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
+                    ***REMOVED***
+                        Url = $"attachment://***REMOVED***Path.GetFileName(headIconFileName[0])***REMOVED***"
+                  ***REMOVED***,
+
+                    Color = new Optional<DiscordColor>(new DiscordColor(advice.Color)),
+              ***REMOVED***;
+
+                if (!string.IsNullOrEmpty(advice.SecondaryNotes))
+                ***REMOVED***
+                    string[] notesTitles = ReturnNotesTitle(advice);
+
+                    embed.AddField(notesTitles[0], BuildNotes(advice));
+
+                    embed.AddField(notesTitles[1], string.Concat("```\n", advice.SecondaryNotes, "```"));
+
+                    if(!string.IsNullOrEmpty(advice.TertiaryNotes))
+                        embed.AddField(notesTitles[2], string.Concat("```\n", advice.TertiaryNotes, "```"));
+              ***REMOVED***
+                else
+                ***REMOVED***
+                    embed.AddField("Notes", BuildNotes(advice));
+              ***REMOVED***
+
+                embed.AddField("Stages (ordered from best to worst)", BuildStages(advice));
+                embed.AddField("Documents", BuildOtherFields(advice.Document));
+                embed.AddField("Videos", BuildOtherFields(advice.Video));
+                embed.AddField("Other", BuildOtherFields(advice.Other));
+
+                var msg = await new DiscordMessageBuilder()
+                        .WithEmbed(embed)
+                        .WithFiles(new Dictionary<string, Stream>() ***REMOVED*** ***REMOVED*** $"***REMOVED***Path.GetFileName(headIconFileName[0])***REMOVED***", fs ***REMOVED*** ***REMOVED***)
+                        .SendAsync(ctx.Channel);
           ***REMOVED***
       ***REMOVED***
 
