40c18d69753a2b2895c1b31e417e85cf59fe713a
JayJayDoubleYou <ipmdeluxe@gmail.com>
2021-06-05 17:20:12 +0200

    Added custom statuses, changes in intervals

--- Bot.cs
+++ Bot.cs
@@ -7,6 +7,9 @@ using System.Text;
 using Newtonsoft.Json;
 using DSharpPlus.CommandsNext.Exceptions;
 using tec_xx.Commands;
+using DSharpPlus.Entities;
+using System;
+using System.Timers;
 
 namespace tec_xx
 ***REMOVED***
@@ -15,17 +18,20 @@ namespace tec_xx
         public DiscordClient Client ***REMOVED*** get; private set; ***REMOVED***
         public CommandsNextExtension Commands ***REMOVED*** get; private set; ***REMOVED***
 
+        private static string[] activityMessages = new string[4] ***REMOVED*** "Dreaming in digital", "Living in realtime", "Thinking in binary", "Talking in IP" ***REMOVED***;
+
+        private Timer timer;
+
         public async Task RunAsync()
         ***REMOVED***
             var json = string.Empty;
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
 
             using (var fs = File.OpenRead("config.json"))
             ***REMOVED***
                 using var sr = new StreamReader(fs, new UTF8Encoding(false));
                 json = await sr.ReadToEndAsync().ConfigureAwait(false);
           ***REMOVED***
-
-            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
 
             var config = new DiscordConfiguration
             ***REMOVED***
@@ -35,10 +41,17 @@ namespace tec_xx
                 MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
           ***REMOVED***;
 
-            Client = new DiscordClient(config);
+            timer = new Timer()
+            ***REMOVED***
+                AutoReset = true,
+                Interval = 10000
+          ***REMOVED***;
 
+            Client = new DiscordClient(config);
             Client.Ready += OnClientReady;
 
+            timer.Elapsed += SetActivityStatus;
+            
             var commandsConfig = new CommandsNextConfiguration
             ***REMOVED***
                 StringPrefixes = new string[] ***REMOVED*** configJson.Prefix ***REMOVED***,
@@ -58,9 +71,22 @@ namespace tec_xx
 
         private Task OnClientReady(DiscordClient client, ReadyEventArgs e)
         ***REMOVED***
+            timer.Start();
             return Task.CompletedTask;
       ***REMOVED***
 
+        private async void SetActivityStatus(object source, ElapsedEventArgs e)
+        ***REMOVED***
+            var activity = new DiscordActivity();
+
+            var random = new Random();
+
+            int index = random.Next(0, activityMessages.Length);
+
+            activity.Name = activityMessages[index];
+
+            await Client.UpdateStatusAsync(activity);
+      ***REMOVED***
 
   ***REMOVED***
 ***REMOVED***
